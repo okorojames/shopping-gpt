ee1d42adf41375d3d27e8267eed3471bd39616a2
Okoro James <okorojameschizaram@gmail.com>
2023-04-29 03:01:11 +0100

    first init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app--container">
+        <Routes></Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
