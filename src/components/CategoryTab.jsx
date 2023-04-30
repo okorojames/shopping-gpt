4890fd2602b34df2253e445812c15b878e4e1bb9
Okoro James <okorojameschizaram@gmail.com>
2023-04-30 10:43:06 +0100

    working on the tab component for product categroy filter

--- src/components/CategoryTab.jsx
+++ src/components/CategoryTab.jsx
@@ -1,14 +1,126 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const CategoryTab = () => {
+  const all_categories = useRef();
+  const tab_chevron = useRef();
+  const toggleCategory = () => {
+    all_categories.current.classList.toggle("toggle--categories");
+    tab_chevron.current.classList.toggle("toggle--categories");
+  };
   return (
     <div className="category--tab">
-      <div className="category--tab--h1">Filters</div>
+      <div
+        className="category--tab--h1"
+        style={{ fontWeight: "600", marginBottom: "5px", fontSize: "18px" }}
+      >
+        Filters
+      </div>
       <div className="filters--row">
         <div className="categories">
-          <div className="category--text">Categories</div>
-          <div className="all--categories">
-            <div></div>
+          <div
+            className="category--text"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              cursor: "pointer",
+              opacity: ".5",
+            }}
+            onClick={toggleCategory}
+          >
+            <div style={{ fontWeight: "500" }}>Categories</div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="tab-category-chevron"
+              style={{ height: "24px", width: "24px" }}
+              ref={tab_chevron}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div>
+          <div
+            className="all--categories"
+            ref={all_categories}
+            style={{ marginTop: "15px" }}
+          >
+            <form>
+              {/* check one */}
+              <input
+                type="checkbox"
+                id="computer"
+                style={{ cursor: "pointer" }}
+              />
+              <label
+                htmlFor="computer"
+                style={{
+                  marginLeft: "6px",
+                  cursor: "pointer",
+                  marginBottom: "8px",
+                }}
+              >
+                Computer Accessories
+              </label>
+              <br />
+              {/* check two */}
+              <input
+                type="checkbox"
+                id="electronics"
+                style={{ cursor: "pointer" }}
+              />
+              <label
+                htmlFor="electronics"
+                style={{
+                  marginLeft: "6px",
+                  cursor: "pointer",
+                  marginBottom: "8px",
+                }}
+              >
+                Electronics
+              </label>
+              <br />
+              {/* check three */}
+              <input
+                type="checkbox"
+                id="clothings"
+                style={{ cursor: "pointer" }}
+              />
+              <label
+                htmlFor="clothings"
+                style={{
+                  marginLeft: "6px",
+                  cursor: "pointer",
+                  marginBottom: "8px",
+                }}
+              >
+                Clothings
+              </label>
+              <br />
+              {/* check four */}
+              <input
+                type="checkbox"
+                id="jewelries"
+                style={{ cursor: "pointer" }}
+              />
+              <label
+                htmlFor="jewelries"
+                style={{
+                  marginLeft: "6px",
+                  cursor: "pointer",
+                  marginBottom: "8px",
+                }}
+              >
+                Jewelries
+              </label>
+              <br />
+            </form>
           </div>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import userIcon from "../assets/images/user-icon.jpg";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [itemNumber, setItemNumber] = useState(3);
+  const [itemNumber, setItemNumber] = useState(5);
   return (
     <header>
       <nav>
@@ -86,9 +86,17 @@ const Header = () => {
                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
               />
             </svg>
-            <i className="fa-solid fa-bag-shopping shopping--bag--icon">
+            <div className="cart--box">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="shopping--bag--icon"
+              >
+                <path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z" />
+              </svg>
               <div className="item--numbers">{itemNumber}</div>
-            </i>
+            </div>
+
             <div className="nav--user--profile">
               <img src={userIcon} alt="" className="nav--user--profile--img" />
             </div>
